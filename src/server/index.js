6394121ccf67460743403cd03bab7f160355bc09
Silvan Haeussermann <silvan.haeussermann@daimler.com>
2018-08-23 15:56:08 +0200

    Updated index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,22 @@
-import startServer from './api';
+var express = require('express');
+var path = require('path');
+var app = express();
 
-console.log('Starting Pottpokal API...');
-startServer();
+// Server static content for public and node_modules folder
+var publicPath = path.resolve(__dirname, "../../public");
+app.use(express.static(publicPath));
+var nodePath = path.resolve(__dirname, "../../node_modules");
+app.use('/modules', express.static(nodePath));
+
+// Serve index.html
+app.get('/', function(req, res) {
+    res.sendFile(publicPath + "/index.html");
+});
+
+// Start server
+var server = app.listen(8088, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Server is now listening at %s:%s", host, port);
+})
