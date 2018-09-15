d07a38e7f9ea20efd170e738ddcf6f69885cfee6
Silvan Haeussermann <silvan.haeussermann@daimler.com>
2018-09-15 13:26:15 +0200

    Merge

--- src/common/actions/index.js
+++ src/common/actions/index.js
@@ -1,5 +1,4 @@
 export const LOGIN = 'LOGIN';
-<<<<<<< HEAD
 export const LOGOUT = 'LOGOUT';
 
 const checkLogin = (username, password) => {
@@ -24,14 +23,4 @@ export function doLogout() {
         type: LOGOUT,
         user
     }
-=======
-
-export function doLogin(name) {
-    console.log('Do Login');
-
-    return {
-        type: LOGIN,
-        name: name,
-    };
->>>>>>> 8f9e86a117c724d53dceba07c7534028c02970c8
 }
